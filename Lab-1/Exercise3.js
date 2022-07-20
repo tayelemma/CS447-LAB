23e3d4335605d80ab49f92948c14cbbb284a549c
Taye Lemma <lemmataye@yahoo.com>
2022-07-20 14:32:39 -0500

    Lab-1

--- Lab-1/Exercise3.js
+++ Lab-1/Exercise3.js
@@ -1,7 +1,8 @@
 // Fix the slow function to be asynchronous/non-blocking
 function slow(callback) {
-    for (let i = 0; i <= 5e8; i++) { }
-    if (Math.random() > 0.5) {
+    // for (let i = 0; i <= 5e8; i++) { }
+    let random = Math.random();
+    if (random > 0.5) {
         return callback("Error", null)
     }
     return callback(null, { id: 12345 })
@@ -10,14 +11,26 @@ function slow(callback) {
 function exec(fn) {
     // Complete the code here to implement chaining with callback
     // have to have an object that has done and fail
-    return {
-        done: function (fu) {
-
-        },
-        fail: function (fu) {
-
-        }
+    function foo(arg1, arg2){
+        return arg1? arg1 : arg2;
     }
+    const resut = fn(foo);
+
+    let obj = {
+        done: function(callback){
+            if(resut != 'Error'){
+                callback(resut);
+            }
+            return this;
+        },
+        fail: function(callback){
+            if(resut === 'Error'){
+                callback(resut);
+            }
+            return this;
+        }
+    };
+    return obj;
 }
 
 exec(slow)
